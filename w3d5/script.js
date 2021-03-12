90b9101869e8d9bc4b97db7d0a7bf3d3b04b1e19
danielethi <danielethi@outlook.com>
2021-03-12 16:15:53 -0600

    simple DOM calc

---
+++ w3d5/script.js
@@ -0,0 +1,14 @@
+function sum(){
+  let x = doucment.getElementById("inp1").value;
+
+
+  document.getElementById('plus').innerHTML=x;
+
+
+
+}
+function onclick(){
+         let y=document.getElementById("calc");
+
+
+}
