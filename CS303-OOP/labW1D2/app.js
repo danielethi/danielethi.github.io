09de8f1b90467651e1d0c37d3adc4d72cc6e2e8d
danielethi <danielethi@outlook.com>
2021-03-23 15:59:01 -0500

    OOP

--- CS303-OOP/labW1D2/app.js
+++ CS303-OOP/labW1D2/app.js
@@ -4,11 +4,38 @@
 
 // See tests.js for behavior
 function nameString(obj) {
-    if(Object.keys(obj).length===0){
+    // if(Object.keys(obj).length===0){
+    //     return "object has no properties";
+    // }else{
+    //     return obj;
+    // }
+    // if (obj === undefined) {
+    //     return "no object passed";
+    // }else if(obj===null){
+    //     return "null arguement";
+    // }else{
+
+    // }
+    function nameString(obj) {
+        if (obj === undefined) {
+        return "no object passed";
+        } else if (obj === null) {
+        return "null argument";
+        } else {
+        if (Object.keys(obj).length === 0) {
         return "object has no properties";
-    }else{
-        return obj;
-    }
+        } else {
+        let keys = [];
+        let result;
+        for (let key in obj) {
+        keys.push(key); //[a,z,m]
+        result = keys.sort().join("."); //[a,m,z]
+        //"a.m.z" 
+        }
+        return result;
+        }
+        }
+       }
 
    }
   
