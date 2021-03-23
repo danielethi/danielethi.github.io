280ccaec42c50421646020a05d420f08f0f6305b
danielethi <danielethi@outlook.com>
2021-03-23 16:10:01 -0500

    OOP

--- CS303-OOP/labW1D2/app.js
+++ CS303-OOP/labW1D2/app.js
@@ -16,28 +16,29 @@ function nameString(obj) {
     // }else{
 
     // }
-    function nameString(obj) {
-        if (obj === undefined) {
-        return "no object passed";
-        } else if (obj === null) {
-        return "null argument";
-        } else {
-        if (Object.keys(obj).length === 0) {
-        return "object has no properties";
-        } else {
-        let keys = [];
-        let result;
-        for (let key in obj) {
-        keys.push(key); //[a,z,m]
-        result = keys.sort().join("."); //[a,m,z]
-        //"a.m.z" 
+  
+            if (obj === undefined) {
+            return "no object passed";
+            } else if (obj === null) {
+            return "null argument";
+            } else {
+            if (Object.keys(obj).length === 0) {
+            return "object has no properties";
+            } else {
+            let keys = [];
+            let result;
+            for (let key in obj) {
+            keys.push(key); //[a,z,m]
+            result = keys.sort().join("."); //[a,m,z]
+            //"a.m.z" 
+            }
+            return result;
+            }
+            }
+           
         }
-        return result;
-        }
-        }
-       }
-
-   }
+    
+   
   
 
   
