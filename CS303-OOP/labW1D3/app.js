ee56f37c20c7cd9039672d00163f74e19ad8fb3c
danielethi <danielethi@outlook.com>
2021-03-24 18:49:44 -0500

    OOP

--- CS303-OOP/labW1D3/app.js
+++ CS303-OOP/labW1D3/app.js
@@ -1,4 +1,86 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D3";
-}
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+/*
+Function analyzer
+Has an unbound this
+Creates an object that contains the following information from the "this" object.
+{
+	numProperties  	// number of properties
+	cntShortName	// count of property names shorter than 3 characters
+	cntReferences	// count of reference properties (use the typeof operator to compute this)
+}	
+*/
+// let person = new Person(name,country,grades)
+
+// function analyzer() {
+// 	numProperties(){
+// 	let count=0;
+// 	for(let key in person){
+// 		count++
+	
+//     return count;
+// }
+
+
+// person.f=analyzer;
+// person.x=0;
+// let analysis=person.f()
+
+// (analysis.numProperties, 6);
+// (analysis.cntShortName, 2);
+// (analysis.cntReference, 1);
+
+function analyzer() {
+	let output = {};
+	let k = Object.keys(this);
+	let v = Object.values(this);
+
+	output.numProperties = k.length;
+  
+	let countNum = 0;
+	for (let elem of k) {
+	 
+	  if (elem.length < 3) countNum++;
+	}
+	output.cntShortName = countNum;
+  
+	let countObj = 0;
+	for (let elem of v) {
+	 
+	  if (typeof elem === "object") {
+		countObj++;
+	  }
+	}
+	output.cntReference = countObj;
+  
+	return output;
+  }
+  
+
+/* Constructor for a person object
+	Person(name, country, grades) creates object
+	{
+		name: name,  // person's name
+		county: country, // person's country
+		grades: grades,  // integer array of grades
+		computeGrade     // function that computes the grade from grades
+	}
+*/
+
+function Person(name, country, grades) {
+	this.name = name;
+	this.country = country;
+	this.grades = grades;
+	this.computeGrade = function () {
+	let sum = 0;
+	for (let grade of this.grades) {
+	sum += grade;
+	}
+	let avg = sum / this.grades.length;
+	return avg;
+	}
+   }
+	
+
