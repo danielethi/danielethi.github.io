10f9cab7690a993344df1eca4b5c8ff28ebc60ab
danielethi <danielethi@outlook.com>
2021-04-04 18:46:54 -0500

    OOP

---
+++ CS303-OOP/timer/timer.js
@@ -0,0 +1,10 @@
+// Write a function printNumbers(from, to) that outputs a number every second, starting from
+//from and ending with to
+
+function printNumbers(from, to){
+    for(let i=from;i<to;i++){
+         console.log(i)
+    }
+}
+
+setInterval(printNumbers,1000,4,9)
