371982a8aee7c85e0f31ee02e7f5528a8c61edaa
danielethi <danielethi@outlook.com>
2021-04-04 19:33:59 -0500

    OOP

---
+++ CS303-OOP/timer/timer2.js
@@ -0,0 +1,10 @@
+// Any setTimeout will run only after the current code has finished.
+
+let i = 0;
+
+setTimeout(() => alert(i), 100); // ?
+
+// assume that the time to execute this function is >100ms
+for(let j = 0; j < 100000000; j++) {
+  i++;
+}
